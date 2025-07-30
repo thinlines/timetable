8b0277cccf42fad9f8094e50196cd123d781443b
Randy Josleyn <randy.josleyn@gmail.com>
2025-07-30 05:07:38 -0600

    update config with more realistic schedule

--- timetable.py
+++ timetable.py
@@ -2,13 +2,14 @@
 """Wrapper script that generates a timetable using OR-Tools."""
 
 from timetable_ortools import load_config, solve_timetable
-from schedule_output import output_timetable
+from schedule_output import output_timetable, output_teacher_schedule
 
 
 def main() -> None:
     cfg = load_config()
     timetable = solve_timetable(cfg)
     output_timetable(timetable)
+    output_teacher_schedule(timetable, "Serena")
 
 
 if __name__ == "__main__":
